52fdb4fb693241a0a07972b7e369e45406c52916
Carlos Alexandre <contatocarlosalexandrecb@gmail.com>
2024-04-28 23:57:23 -0300

    Update 1.0.5
    - Novo enum OrderStatus,
    - Novos dados na classe TestConfig,
    - Novos dados na classe Order.

--- src/main/java/com/meucursoudemy/course/config/TestConfig.java
+++ src/main/java/com/meucursoudemy/course/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.meucursoudemy.course.config;
 
 import com.meucursoudemy.course.entities.Order;
 import com.meucursoudemy.course.entities.User;
+import com.meucursoudemy.course.entities.enums.OrderStatus;
 import com.meucursoudemy.course.repositories.OrderRepository;
 import com.meucursoudemy.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2024-02-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-03-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-04-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-02-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2024-03-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-04-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
