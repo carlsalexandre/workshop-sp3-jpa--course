65109a63f3dc216f177fef697ebb56169c65ba1c
Carlos Alexandre <contatocarlosalexandrecb@gmail.com>
2024-05-01 23:55:15 -0300

    1.0.7,1
    - Nova classe OrderItemRepository,
    - Novos dados em Order e OrderItem,
    - Novos dados em TestConfig.

--- src/main/java/com/meucursoudemy/course/config/TestConfig.java
+++ src/main/java/com/meucursoudemy/course/config/TestConfig.java
@@ -1,15 +1,9 @@
 package com.meucursoudemy.course.config;
 
 
-import com.meucursoudemy.course.entities.Category;
-import com.meucursoudemy.course.entities.Order;
-import com.meucursoudemy.course.entities.Product;
-import com.meucursoudemy.course.entities.User;
+import com.meucursoudemy.course.entities.*;
 import com.meucursoudemy.course.entities.enums.OrderStatus;
-import com.meucursoudemy.course.repositories.CategoryRepository;
-import com.meucursoudemy.course.repositories.OrderRepository;
-import com.meucursoudemy.course.repositories.ProductRepository;
-import com.meucursoudemy.course.repositories.UserRepository;
+import com.meucursoudemy.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,5 +65,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
