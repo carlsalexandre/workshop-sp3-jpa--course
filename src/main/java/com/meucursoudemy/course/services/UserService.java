bce20e554eb892092deb76e64588e9c467d8a190
Carlos Alexandre <contatocarlosalexandrecb@gmail.com>
2024-05-07 19:39:22 -0300

    1.0.9
    - Novos dados em UserResouce, User Service, Order, OrderItem e Payment,
    - Novas classes de exception.

--- src/main/java/com/meucursoudemy/course/services/UserService.java
+++ src/main/java/com/meucursoudemy/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.meucursoudemy.course.services;
 
 import com.meucursoudemy.course.entities.User;
 import com.meucursoudemy.course.repositories.UserRepository;
+import com.meucursoudemy.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,26 @@ public class UserService {
 
     public User findbyId(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(( ) -> new ResourceNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
---
+++ src/main/java/com/meucursoudemy/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.meucursoudemy.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
